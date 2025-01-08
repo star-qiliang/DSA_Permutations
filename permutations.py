dc55acd65e057b5a14525f7e27e68ccca1d56ac8
qiliang.huang <goosehuangmatt@gmail.com>
2025-01-08 17:50:28 +0800

    refactor permutation function for clarity and efficiency

--- permutations.py
+++ permutations.py
@@ -1,17 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def get_res(nums):
-            if len(nums)==1:
-                return [nums,]
+
+        def get_combination(a_list):
+            if not a_list:
+                return [[]]
+            if len(a_list)==1:
+                return [a_list,]
 
             res_list = []
-            for i in range(len(nums)):
-                tmp_nums = nums[:i] + nums[i+1:]
-                tmp_list = get_res(tmp_nums)
-                tmp_list = [[nums[i],]+x for x in tmp_list]
-                res_list+=tmp_list
+            for i in range(len(a_list)):
+                tmp_list = a_list[:i] + a_list[i+1:]
+                tmp_res_list = get_combination(tmp_list)
+                tmp_res_list = [[a_list[i]]+x for x in tmp_res_list]
+                res_list += tmp_res_list
 
             return res_list
 
-        res_list = get_res(nums)
+        res_list = get_combination(nums)
+
         return res_list
+
